72a6cc316cc92768b9a63bcfee7a77e9526edc6d
mike3run <mpalauzarza@gmail.com>
2015-12-06 18:35:39 -0600

    cambios de limpieza en gulpflie

--- gulpfile.js
+++ gulpfile.js
@@ -139,7 +139,7 @@ gulp.task('default', ['browsersync', 'watch']);
 
 // Un archivo hermoso de limpieza mu
 gulp.task('clean', function() {
-  return clean(['./dist/**', '!./dist']);
+  return clean(['./dist', './.publish']);
 });
 
 
