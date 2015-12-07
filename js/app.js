6495d54ac31fa90e57d41aac2412b36695c430e6
mike3run <mpalauzarza@gmail.com>
2015-12-06 18:23:05 -0600

    Update 2015-12-07T00:23:04.150Z

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/*!
+ * Manila un stream de Gulp por: @mpalau
+ * Inspirado en CodeKit pero queriendo hacer más!
+ * Version 1.0
+ * Copyright 2015
+ *
+ * http://miguel.click/ 😜
+ */
+function amistad(o){console.log("Somos "+o+" amigos!")}console.log("Estamos funcionando perritas!"),amistad(2);
