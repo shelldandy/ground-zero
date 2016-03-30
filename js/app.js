cdafdbc9ca00012b053c2209dd35d8037514ae68
Miguel Palau <mpalauzarza@gmail.com>
2016-03-30 14:53:35 -0600

    Update 2016-03-30T20:53:30.105Z

--- js/app.js
+++ js/app.js
@@ -1,9 +1,1 @@
-/*!
- * Manila un stream de Gulp por: @mpalau
- * Inspirado en CodeKit pero queriendo hacer más!
- * Version 1.0
- * Copyright 2015
- *
- * http://miguel.click/ 😜
- */
-function amistad(o){console.log("Somos "+o+" amigos!")}console.log("Estamos funcionando perritas!"),amistad(2);
+(function(){console.log("La vida es más que bella, es hermosa")}).call(this);
