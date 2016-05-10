de5d5585370c4b8ff86690ed01ee4d845b6f3638
mike3run <mpalauzarza@gmail.com>
2016-05-09 20:12:48 -0500

    Update 2016-05-10T01:12:24.216Z

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-(function(){console.log("La vida es más que bella, es hermosa")}).call(this);
+(function() {
+  console.log('La vida es más que bella, es hermosa');
+
+}).call(this);
+
+//# sourceMappingURL=app.js.map
