1188e5825aed8cad3432d8fcc4519ac0657671ef
Miguel Palau <mpalauzarza@gmail.com>
2016-08-21 18:20:54 -0500

    fixed fonts task

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -6,11 +6,11 @@ const config        = require('../config');
 const onError       = require('./error');
 
 gulp.task('fonts', done => {
-  return gulp.src('config.fonts')
+  return gulp.src(config.fonts)
   .pipe( $.plumber( {errorHandler : onError} ) )
   .pipe( $.cache( $.cssfont64() ) )
   .pipe( $.concat('fonts.css') )
-  .pipe( gulp.dest(`${config.exportPath}/assets/stylesheets`) );
+  .pipe( gulp.dest(`${config.distFolder}/assets/stylesheets`) );
 
   done();
 
