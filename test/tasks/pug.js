b917f544fae3efd334df6bf6f6272560d53cbfb8
Diego Benjamín Aguilar Aguilar <diegoaguilaraguilar@gmail.com>
2016-09-10 01:49:29 -0500

    Simplify tests

--- test/tasks/pug.js
+++ test/tasks/pug.js
@@ -6,30 +6,28 @@ const expect = chai.expect;
 
 describe('pug gulp task tests suite', function () {
 
+  beforeEach(function (done) {
+    utils.runTask('pug', done);
+  });
+
   afterEach(function (done) {
     utils.runTask('clean', done);
   });
 
   it('Should create a an index.html', function (done) {
-    utils.runTaskAndAssertFile('pug', 'index.html', './dist', done);
+    utils.files.fileExists('index.html', './dist', done);
   });
 
   it('Should transpile a valid html', function (done) {
-    utils.runTask('pug', function (taskErr, results) {
-      if (taskErr) {
-        return done(taskErr);
+    utils.files.loadHTMLIndex(function (err, $) {
+      if (err) {
+        return done(err);
       } else {
-        utils.files.loadHTMLIndex(function (err, $) {
-          if (err) {
-            return done(err);
-          } else {
-            let title = $('head title').text();
-            let svg = $('.chochini');
-            expect(title).to.equal('Ground Zero');
-            expect(svg.is('svg')).to.be.true;
-            done();
-          }
-        });
+        let title = $('head title').text();
+        let svg = $('.chochini');
+        expect(title).to.equal('Ground Zero');
+        expect(svg.is('svg')).to.be.true;
+        done();
       }
     });
   });
