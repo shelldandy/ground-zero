248cb843f469ee04731101151a7e1788c515d2e9
Miguel Palau <mpalauzarza@gmail.com>
2017-05-14 20:48:01 -0500

    starting to add browserify

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,32 +1,59 @@
 'use strict'
+
 const gulp = require('gulp')
-const plugins = require('gulp-load-plugins')
-const $ = plugins()
 const config = require('../config')
+const $ = require('gulp-load-plugins')()
 const when = require('gulp-if')
 
-// Check if gulp scripts --prod or --production has been added to the task
-const argv = require('yargs').argv
-const production = argv.prod || argv.production
+const production = config.production
+const destination = config.directories.dist.scripts
 
-const destination = `${config.distFolder}/assets/javascript`
+const browserify = require('browserify')
+const source = require('vinyl-source-stream')
+const buffer = require('vinyl-buffer')
+const babel = require('babelify')
 
 gulp.task('scripts', () => {
-  return gulp.src(config.scriptFiles)
-  .pipe(when(!production, $.sourcemaps.init()))
-  .pipe($.concat('main.js'))
-  .pipe(when(!production, $.sourcemaps.write('./')))
-  .pipe(gulp.dest(destination))
+  const bundler = browserify({
+    entries: './src/assets/js/index.js',
+    debug: true,
+    transform: [
+      babel.configure({
+        'presets': [
+          [
+            'env',
+            {
+              'targets': {
+                'browsers': ['last 2 versions', 'iOS 8'],
+                'uglify': 2
+              }
+            }
+          ]
+        ],
+        'plugins': [
+          [
+            'transform-object-rest-spread',
+            {
+              'useBuiltIns': true
+            }
+          ]
+        ]
+      })
+    ]
+  })
 
-  // All production stuff here
-
-  // Rename file to .min and uglify that stuff
-  .pipe(when(production, $.rename({suffix: '.min'})))
-  .pipe(when(production, $.uglify({
-    preserveComments: 'license'
-  }))).on('error', config.errorHandler)
-  .pipe(when(production, gulp.dest(destination)))
-  // Finally make it uber small with gzip
-  .pipe(when(production, $.gzip()))
-  .pipe(when(production, gulp.dest(destination)))
+  return bundler.bundle()
+    .on('error', config.onError)
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(when(!production, $.sourcemaps.init({ loadMaps: true })))
+    .pipe(when(!production, $.sourcemaps.write('./')))
+    .pipe(gulp.dest(destination))
+    // All production stuff here
+    // Rename file to .min and uglify that stuff
+    .pipe(when(production, $.rename({suffix: '.min'})))
+    .pipe(when(production, $.uglify({
+      preserveComments: 'license'
+    }))).on('error', config.onError)
+    .pipe(when(production, gulp.dest(destination)))
 })
